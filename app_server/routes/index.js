cb6dd5b47aeda234fa313167d26cae56287e3711
Corey Creech <c.creech@snhu.edu>
2025-09-10 17:11:14 -0400

    Added route for travel page

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const ctrMain = require('../controllers/main');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrMain.index);
 
 module.exports = router;
